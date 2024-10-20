4b012bdd54348a0e1486794a77896e85ace4a0c1
Ramonfvm <ramonfhelipe547@gmail.com>
2024-10-19 21:50:48 -0300

    adição da api

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,13 +1,37 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import Styles from './Search.module.css';
-import { Axios } from 'axios';
+import axios from 'axios';
 
 export function Search() {
+    const Api_key = import.meta.env.VITE_OMDB_API_KEY;
+    const [movie, setMovie] = useState('');
 
-    
+
+    useEffect(() => {
+
+        
+        if (movie) {
+            axios.get(`http://www.omdbapi.com/?t=${movie}&apikey=${Api_key}`)
+                .then(response => {
+
+                    console.log(response);
+                   
+                })
+        }
+    }, [movie, Api_key]);
     return (
         <div className={Styles.Search}>
-            <input type="text"  placeholder="Pesquisar..." />
+            <input
+                type="text"
+                value={movie}
+                placeholder="Pesquisar..."
+                onChange={(e) => setMovie(e.target.value)}
+
+            />
+
+
         </div>
+
+
     );
 }
