df91973d01bd8fb6727049014443091d1a9e75e0
Ramonfvm <ramonfhelipe547@gmail.com>
2024-10-20 02:28:05 -0300

    implementando a logica da api

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -5,33 +5,55 @@ import axios from 'axios';
 export function Search() {
     const Api_key = import.meta.env.VITE_OMDB_API_KEY;
     const [movie, setMovie] = useState('');
-
+    const [movieData, setMovieData] = useState(null);
 
     useEffect(() => {
+        const fetchMovieData = async () => {
+            if (movie) {
+                try {
+                    const response = await axios.get(`http://www.omdbapi.com/?t=${movie}&apikey=${Api_key}`);
+                    setMovieData(response.data);
+                } catch (error) {
+                    console.error('Erro ao buscar dados do filme:', error);
+                }
+            }
+        };
 
-        
-        if (movie) {
-            axios.get(`http://www.omdbapi.com/?t=${movie}&apikey=${Api_key}`)
-                .then(response => {
-
-                    console.log(response);
-                   
-                })
-        }
+        fetchMovieData();
     }, [movie, Api_key]);
+
     return (
-        <div className={Styles.Search}>
-            <input
-                type="text"
-                value={movie}
-                placeholder="Pesquisar..."
-                onChange={(e) => setMovie(e.target.value)}
+        <div>
+            <div className={Styles.Search}>
+                <input
+                    type="text"
+                    value={movie}
+                    placeholder="Pesquisar..."
+                    onChange={(e) => setMovie(e.target.value)}
+                />
+            </div>
 
-            />
-
-
+            {movieData && (
+                <div className={Styles.MovieInfo}>
+                    <h1>{movieData.Title}</h1>
+                    <p>Ano: {movieData.Year}</p>
+                    <p>Gênero: {movieData.Genre}</p>
+                    <p>Sinopse: {movieData.Plot}</p>
+                    <p>Diretor: {movieData.Director}</p>
+                    <h2>Classificações:</h2>
+                    {movieData.Ratings && movieData.Ratings.length > 0 ? (
+                        <ul>
+                            {movieData.Ratings.map((rating, index) => (
+                                <li key={index}>
+                                    {rating.Source}: {rating.Value}
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>Nenhuma classificação disponível.</p>
+                    )}
+                </div>
+            )}
         </div>
-
-
     );
 }
